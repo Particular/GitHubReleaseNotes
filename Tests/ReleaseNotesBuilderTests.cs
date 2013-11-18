bea22a5410ec5077bc8ba67d467861ea014c352e
andreasohlund <andreasohlund2@gmail.com>
2013-11-18 14:52:22 +0100

    No more case sensitive

--- Tests/ReleaseNotesBuilderTests.cs
+++ Tests/ReleaseNotesBuilderTests.cs
@@ -32,6 +32,18 @@ public class ReleaseNotesBuilderTests
 
     [Test]
     [Explicit]
+    public async void SingleMilestone3()
+    {
+        var gitHubClient = ClientBuilder.Build();
+
+        var releaseNotesBuilder = new ReleaseNotesBuilder(gitHubClient, "Particular", "NServiceBus.SqlServer", "1.1.0");
+        var result = await releaseNotesBuilder.BuildReleaseNotes();
+        Debug.WriteLine(result);
+        ClipBoardHelper.SetClipboard(result);
+    }
+
+    [Test]
+    [Explicit]
     public void OctokitTests()
     {
         var gitHubClient = ClientBuilder.Build();

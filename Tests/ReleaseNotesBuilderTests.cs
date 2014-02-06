a3c0aebb348d6d543a53f0388444e6b5534e0181
Simon Cropp <simon.cropp@gmail.com>
2014-02-06 11:22:09 +1100

    bump refs

--- Tests/ReleaseNotesBuilderTests.cs
+++ Tests/ReleaseNotesBuilderTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Linq;
 using NUnit.Framework;
 using ReleaseNotesCompiler;
 
@@ -38,25 +37,4 @@ public class ReleaseNotesBuilderTests
     }
 
   
-}
-
-[TestFixture]
-public class ReleaseManagerTests
-{
-    [Test]
-    [Explicit]
-    public async void List_releases_that_needs_updates()
-    {
-        var gitHubClient = ClientBuilder.Build();
-
-        var releaseNotesBuilder = new ReleaseManager(gitHubClient, "Particular");
-        var result = await releaseNotesBuilder.GetReleasesInNeedOfUpdates();
-
-        Debug.WriteLine("{0} releases found that needs updating",result.Count());
-        foreach (var releaseName in result)
-        {
-            Debug.WriteLine(releaseName);
-        }
-       
-    }  
 }

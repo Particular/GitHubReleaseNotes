8f1ae418b258629c2c3b16a2439d043a86255f81
nulltoken <emeric.fermas@gmail.com>
2014-02-10 11:40:58 +0100

    Teach ClientBuilder to honor potential proxy

--- Tests/ClientBuilder.cs
+++ Tests/ClientBuilder.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using Octokit;
+using Octokit.Internal;
 
 public static class ClientBuilder
 {
     public static GitHubClient Build()
     {
+        var credentialStore = new InMemoryCredentialStore(Helper.Credentials);
 
+        var httpClient = new HttpClientAdapter(Helper.Proxy);
 
-        var githubUsername = Environment.GetEnvironmentVariable("OCTOKIT_GITHUBUSERNAME");
-        var githubPassword = Environment.GetEnvironmentVariable("OCTOKIT_GITHUBPASSWORD");
+        var connection = new Connection(
+            new ProductHeaderValue("ReleaseNotesCompiler"),
+            GitHubClient.GitHubApiUrl,
+            credentialStore,
+            httpClient,
+            new SimpleJsonSerializer());
 
-        if (githubUsername == null || githubPassword == null)
-        {
-            throw new Exception("expected OCTOKIT_GITHUBUSERNAME and OCTOKIT_GITHUBPASSWORD env variables to exist");
-        }
+        var client = new GitHubClient(connection);
 
-        return new GitHubClient(new ProductHeaderValue("ReleaseNotesCompiler"))
-        {
-            Credentials = new Credentials(githubUsername, githubPassword)
-        };
+        return client;
     }
 }

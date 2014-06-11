7d6b6dd3383e658e1628b8652f749aff8e990f2f
Cameron MacFarland <distantcam@gmail.com>
2014-06-11 16:10:47 +0800

    Added asset to release compiler. Fixed name, made release draft

--- App/Program.cs
+++ App/Program.cs
@@ -1,35 +1,37 @@
 ﻿namespace ReleaseNotesCompiler.CLI
 {
     using System;
+    using System.IO;
     using System.Threading.Tasks;
     using Octokit;
+    using FileMode = System.IO.FileMode;
 
     class Program
     {
         static int Main(string[] args)
         {
-            if (args.Length != 5)
+            if (args.Length != 6)
             {
                 PrintHelp();
                 return 1;
             }
 
-            return MainAsync(args[0], args[1], args[2], args[3], args[4]).Result;
+            return MainAsync(args[0], args[1], args[2], args[3], args[4], args[5]).Result;
         }
 
         static void PrintHelp()
         {
-            Console.WriteLine("Usage: ReleaseNotesBuilder.CLI <username> <password> <owner> <repo> <milestone>");
+            Console.WriteLine("Usage: ReleaseNotesBuilder.CLI <username> <password> <owner> <repo> <milestone> <asset>");
         }
 
-        static async Task<int> MainAsync(string username, string password, string owner, string repository, string milestone)
+        static async Task<int> MainAsync(string username, string password, string owner, string repository, string milestone, string asset)
         {
             try
             {
                 var creds = new Credentials(username, password);
                 var github = new GitHubClient(new ProductHeaderValue("ReleaseNotesCompiler")) { Credentials = creds };
 
-                await CreateRelease(github, owner, repository, milestone);
+                await CreateRelease(github, owner, repository, milestone, asset);
 
                 return 0;
             }
@@ -41,7 +43,7 @@
             }
         }
 
-        private static async Task CreateRelease(GitHubClient github, string owner, string repository, string milestone)
+        private static async Task CreateRelease(GitHubClient github, string owner, string repository, string milestone, string asset)
         {
             var releaseNotesBuilder = new ReleaseNotesBuilder(github, owner, repository, milestone);
 
@@ -49,11 +51,15 @@
 
             var releaseUpdate = new ReleaseUpdate(milestone)
             {
-                Draft = false,
+                Draft = true,
                 Body = result,
-                Name = string.Format("{0} {1}", repository, milestone)
+                Name = milestone
             };
-            await github.Release.CreateRelease(owner, repository, releaseUpdate);
+            var release = await github.Release.CreateRelease(owner, repository, releaseUpdate);
+
+            var upload = new ReleaseAssetUpload { FileName = Path.GetFileName(asset), ContentType = "application/octet-stream", RawData = File.Open(asset, FileMode.Open) };
+
+            await github.Release.UploadAsset(release, upload);
         }
     }
 }
